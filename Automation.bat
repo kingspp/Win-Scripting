794db59f7c976f8bac717e5c8775aa5fdeef0d88
kingspp <kingspprathyush@gmail.com>
2014-12-25 23:12:10 -0800

    Added lots of studd

--- Automation.bat
+++ Automation.bat
@@ -1,4 +1,5 @@
-
+:: Innosetup /SILENT /LOG="path"
+:: Nullsoft /S
 goto begin
 
 ::Disable UAC
@@ -9,7 +10,53 @@ C:\Windows\System32\cmd.exe /k %windir%\System32\reg.exe ADD HKLM\SOFTWARE\Micro
 set /p spath="Enter Source Folder: "
 echo %spath%
 
+::7zip
+start /w 7z935.exe /S /LOG="E:/filename.log"
+
+::Aimp3
+start /w  Aimp.exe /AUTO="%programfiles%\AIMP3\" /SILENT 
+
+::Anysend 
+start /w anysend.exe /S
+
+::CD Bank
+start /w  cdbank.exe /S
+
+::CCleaner
+start /w  ccleaner.exe /S 
+
+::Easus Partion Master
+start /w  epm.exe /SILENT /LOG="E:/filename.log"
+
+::FileZilla
+start /w  FileZilla.exe /S 
+
+::JDK
+::jdk-7u51
+::Path set remaining
+start /w  jdk-7u51.exe /quiet
+
+::Notepad++
+start /w  npp.exe /S
+
+::Office 2013
+::Path to be set
+::start /w setup.exe /adminfile config.msp
+
+::Realtek
+::TO do File path
+::start /w  setup.exe /s /sms
+
+::Ultra ISO9
+start /w  ultraiso9.exe /SILENT /LOG="E:/filename.log"
+
+
 :begin
-echo "Hello"
+start /w config.MSP /quiet
+
+
+
+
+
 
  
